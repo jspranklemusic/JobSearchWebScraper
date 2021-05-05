c1d803ac00cb00301d3e14247dfd4c5cbaf33ee5
Josiah Sprankle <jspranklemusic@gmail.com>
2021-05-05 13:26:46 -0500

    added frontend

---
+++ index.js
@@ -0,0 +1,94 @@
+const cheerio = require('cheerio');
+const axios = require('axios').default;
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.static('build'));
+app.use(cors());
+
+class JobSearchObject{
+  constructor(url, query, container, title, company, salary, description,link){
+    this.jobs = [];
+    this.url = url;
+    this.query = query;
+    this.container = container;
+    this.title = title;
+    this.company = company;
+    this.salary = salary;
+    this.description = description;
+  }
+  async populateObject(){
+    const response = await axios.get(this.url+this.query)
+    const $ = cheerio.load(response.data);
+
+    console.log($(this.container).text())
+
+    $(this.container).each((ind,item)=>{
+      const html = $(item).html();
+      const $$ = cheerio.load(html);
+      this.jobs.push({
+        title:$$(this.title) ? $$(this.title).text().replace(/\n/g,'') : null,
+        link:this.url + $$('a').attr("href"),
+        company:$$(this.company) ? $$(this.company).text().replace(/\n/g,'') : null,
+        salary:$$(this.salary) ? $$(this.salary).text().replace(/\n/g,'') : null,
+        description:$$(this.description) ? $$(this.description).text().replace(/\n/g,'') : null
+      })
+    })
+    return this.jobs;
+  }
+}
+
+app.get('/',(req,res)=>{
+    res.sendFile('index.html');
+})
+
+app.get('/api',async (req,res)=>{
+
+  const location = req.query.location || "remote";
+  const terms = req.query.terms || "react";
+
+  const Indeed = new JobSearchObject(
+        url = "https://www.indeed.com",
+        query = `/jobs?q=${terms}&l=${location}`,
+        container='.jobsearch-SerpJobCard',
+        title="h2",
+        company=".company",
+        salary=".salaryText",
+        description=".summary"
+    )
+    const Flexjobs = new JobSearchObject(
+      url="https://www.flexjobs.com",
+      query=`/search?search=&search=${terms}&location=${location}`,
+      container='li.list-group-item.job',
+      title='a.job-link',
+      company='',
+      salary='',
+      description='div.job-details'
+    )
+
+    const SimplyHired = new JobSearchObject(
+      url="https://www.simplyhired.com",
+      query=`/search?q=${terms}&l=${location} `,
+      container="article",
+      title=".jobposting-title",
+      company=".jobposting-company",
+      salary=".jobposting-salary",
+      description=".jobposting-snippet"
+    )
+  
+    const indeedJobs = await Indeed.populateObject();
+    const flexjobsJobs = await Flexjobs.populateObject();
+    const simplyhiredJobs = await SimplyHired.populateObject();
+
+    
+    res.json([...indeedJobs,...flexjobsJobs,...simplyhiredJobs]);
+})
+
+app.listen(PORT, ()=>{
+  console.log("listening on port: " + PORT + "")
+})
+
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+
+  const [data, setData] = useState([])
+  const [location, setLocation] = useState("");
+  const [terms, setTerms] = useState("");
+  const [searching, setSearching] = useState(false);
+
+  async function fetchJobs(event){
+    setSearching(true)
+    event.preventDefault();
+
+    // let url = 'https://jobsearchwebscraper.josiahsprankle.repl.co/?';
+        let url = '/?';
+
+
+    url += 
+      location && terms ? `location=${location}&terms=${terms}` :
+      location && !terms ? `location=${location}` :
+      terms && !location ? `terms=${terms}` : 
+      "";
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data);
+      setSearching(false);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={fetchJobs}>
+      <h1><i class="fas fa-layer-group"></i>&nbsp; My Job Search</h1>
+      
+        <div className="form-control">
+            <label htmlFor="terms">Search terms, skills, or jobs.</label>
+            <input onInput={(e)=>setTerms(e.target.value)} id="terms" type="text" placeholder="Your terms here"/>
+        </div>
+        <div className="form-control">
+          <label htmlFor="location">Search jobs by location</label>
+            <input onInput={(e)=>setLocation(e.target.value)}  id="location" placeholder="You location here" type="text"/>
+        </div>
+        {!searching && <button type="submit"><i class="fas fa-search"></i> Search Jobs</button>}
+        {searching && <button disabled type="submit"><i class="fas fa-compass spin-icon"></i> <span className="fading">Searching...</span></button>}
+        
+      </form>
+      <p className="description"><i class="fas fa-info-circle"></i>&nbsp; Get instant results from companies such as Indeed, Flexjobs, and SimplyHired!</p>
+      <div className="loading-spinner">
+
+      </div>
+      {!searching &&
+      <main className="fade-in">
+       {[...data].map(job=>{
+          return(
+            <div className="job-container">
+              <h2 className="job-title">{ job.title }</h2>
+              <div>
+                <span className="job-company">{ job.company }</span> 
+              </div>
+              <p className="job-description">{ job.description }</p>
+              <div>
+                {job.salary && <em className="job-salary">{ job.salary || "" }</em>  }
+              </div>
+              <a rel="noreferrer" target="_blank" href={job.link}>View Job</a>
+            </div>
+          )
+        })}
+      </main>
+      }
+       
     </div>
   );
 }
